44492e096f9dd99e1c80b89a2c2777bc1d97766c
elLCCH <cristian.98oruro@gmail.com>
2023-04-11 16:26:25 -0400

    finalizando pagina para institutos

--- resources/views/Angular.blade.php
+++ resources/views/Angular.blade.php
@@ -47,7 +47,7 @@
     </script>
 
 
-    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.37973c18af092d4dc5a7.js" defer=""></script></body>
+    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.d1278d76162f0e1e98d2.js" defer=""></script>
 
   <script src="/assets/Angular/assets/pdf-2.10.558.min.js" defer></script>
   <script src="/assets/Angular/assets/viewer-2.10.558.min.js" defer></script>
