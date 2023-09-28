889fc87dc38520c989d02d37811726aa21a71561
elLCCH <cristian.98oruro@gmail.com>
2023-09-28 17:30:17 -0400

    correccion de botones de importacion

--- resources/views/Angular.blade.php
+++ resources/views/Angular.blade.php
@@ -46,7 +46,7 @@
     </script>
 
 
-    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.b644bd1441c6ca52bf53.js" defer=""></script>
+    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.4de619dc3d002d528bbf.js" defer=""></script>
 
   <script src="/assets/Angular/assets/pdf-2.10.558.min.js" defer></script>
   <script src="/assets/Angular/assets/viewer-2.10.558.min.js" defer></script>
