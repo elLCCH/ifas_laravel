6154144b9e6afe61d84f2431b929f734d59eb288
elLCCH <cristian.98oruro@gmail.com>
2023-12-14 19:57:55 -0400

    solucion a las reglas para folklore donde no se seleccionaba correctamente el nivel

--- resources/views/Angular.blade.php
+++ resources/views/Angular.blade.php
@@ -46,7 +46,7 @@
     </script>
 
 
-    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.7ab938da34c5ee1cf58e.js" defer=""></script>
+    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.eb8f6b3f241d8013e79e.js" defer=""></script>
 
   <script src="/assets/Angular/assets/pdf-2.10.558.min.js" defer></script>
   <script src="/assets/Angular/assets/viewer-2.10.558.min.js" defer></script>
