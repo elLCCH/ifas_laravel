5e3d3808a6b0e9a0bd5867aa6e75c135463d7204
elLCCH <cristian.98oruro@gmail.com>
2024-02-15 13:45:35 -0400

    Correccion estadisticas de asig, generacion de Boleta de calificaciones y shop gestion estudiantes

--- resources/views/Angular.blade.php
+++ resources/views/Angular.blade.php
@@ -46,7 +46,7 @@
     </script>
 
 
-    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.70bdd3eaf8c455eec1ef.js" defer=""></script>
+    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.71aad7412789da304e68.js" defer=""></script>
 
   <script src="/assets/Angular/assets/pdf-2.10.558.min.js" defer></script>
   <script src="/assets/Angular/assets/viewer-2.10.558.min.js" defer></script>
