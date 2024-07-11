ce52256f15ebd1f158b0f039b9a3989583c33896
elLCCH <cristian.98oruro@gmail.com>
2024-07-11 01:29:54 -0400

    boton de auto inscripciones deshabilitado

--- resources/views/Angular.blade.php
+++ resources/views/Angular.blade.php
@@ -47,7 +47,7 @@
     </script>
 
 
-    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.b8d164c254b8a28a27ee.js" defer=""></script>
+    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.bf101706b14361682bd6.js" defer=""></script>
 
   <script src="/assets/Angular/assets/pdf-2.10.558.min.js" defer></script>
   <script src="/assets/Angular/assets/viewer-2.10.558.min.js" defer></script>
