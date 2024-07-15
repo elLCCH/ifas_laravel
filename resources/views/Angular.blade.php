6df28b3d418adecd8742ffb943863ca577a7c225
elLCCH <cristian.98oruro@gmail.com>
2024-07-15 12:41:28 -0400

    actualizacion de inscripciones de eventos

--- resources/views/Angular.blade.php
+++ resources/views/Angular.blade.php
@@ -47,7 +47,7 @@
     </script>
 
 
-    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.bf101706b14361682bd6.js" defer=""></script>
+    <script src="/assets/Angular/runtime.8acea97c940ea2f40e7e.js" defer=""></script><script src="/assets/Angular/polyfills.6e24aca16d400ed18e82.js" defer=""></script><script src="/assets/Angular/main.f33b018b168a65e4de4f.js" defer=""></script>
 
   <script src="/assets/Angular/assets/pdf-2.10.558.min.js" defer></script>
   <script src="/assets/Angular/assets/viewer-2.10.558.min.js" defer></script>
